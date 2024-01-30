7ea77792755c53f34f39521b905621f82754ab24
Victoria <vmcnorrill@gmail.com>
2024-01-29 22:28:58 -0500

    broken down steps to fetch+parse data and then to extract individual values

--- generateWeather.sh
+++ generateWeather.sh
@@ -23,5 +23,11 @@ fi
 
 myUrl="https://api.tomorrow.io/v4/weather/realtime?location=${ZIPCODE}&units=imperial&apikey=stq6JSuxCBwvp3u2OVqVXFuQgLTqa8ff"
 
-curl --compressed --request GET --url "$myUrl" --header 'accept: application/json' | jq '.humidity', '.temperature', '.weatherCode.[]', '.windSpeed' | echo "At the moment, the temperature for" $ZIPCODE " is " $temperature "F with " $humidity "% humidity and wind speeds around " $windSpeed "mph. You can expect " $weatherCode "skies."
- 
+# Fetching and parsing the data
+weatherData=$(curl --compressed --request GET --url "$myUrl" --header 'accept: application/json' | jq -r '.')
+
+# Extracting individual values
+temperature=$(echo "$weatherData" | jq -r '.temperature') 
+humidity=$(echo "$weatherData" | jq -r '.humidity')
+weatherCode=$(echo "$weatherData" | jq -r '.weatherCode[0]')
+windSpeed=$(echo "$weatherData" | jq -r '.windSpeed')
